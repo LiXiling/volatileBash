6995489fce3387c3b9cf319149d691b14123f5ad
Maximilian Li <maximilian.xiling.li@gmail.com>
2016-12-03 16:18:44 +0100

    enriched emlwriter

--- volatileIO/EMLWriter.py
+++ volatileIO/EMLWriter.py
@@ -1,13 +1,38 @@
 from FileCreator import FileCreator
 
+DEFAULT_SENDER = "Anna@email.com"
+DEFAULT_RECEIVER = "Bob@email.com"
+DEFAULT_SUBJECT = "This is not a secret Mail"
+DEFAULT_CONTENT = "The secret is herbalTea"
 
 class EMLWriter(FileCreator):
-    def __init__(self, filePath):
+    def __init__(self, filePath,
+                 sender=DEFAULT_SENDER,
+                 receiver = DEFAULT_RECEIVER,
+                 subject = DEFAULT_SUBJECT,
+                 content = DEFAULT_CONTENT):
+
         super(EMLWriter, self).__init__(filePath)
-        self.sender = "sender@email.com"
-        self.receiver = "receiver@email.com"
-        self.content = "The secret is herbalTea"
-        self.subject = "This is not a secret Mail"
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.content = content
+
+    def setSender(self, sender):
+        self.sender = sender
+        return self
+
+    def setReceiver(self, receiver):
+        self.receiver = receiver
+        return self
+
+    def setSubject(self, subject):
+        self.subject = subject
+        return self
+
+    def setContent(self, content):
+        self.content = content
+        return self
 
     def getContent(self):
         return "MIME-Version: 1.0\n" \
